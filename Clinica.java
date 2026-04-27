3f1416e3cc6c91c6a8aa59249458f14d7bf1380a
Adrian Moral <moralmarin2@gmail.com>
2026-04-27 22:31:14 +0200

    Commit3

--- Clinica.java
+++ Clinica.java
@@ -2,46 +2,41 @@ public class Clinica {
 
     public void agendarCita(String paciente, String doctor, String fecha, String tipo) {
 
-        
         if (!validarPaciente(paciente)) return;
 
-        // Lógica de agenda
         System.out.println("Cita agendada para " + paciente + " con el Dr. " + doctor);
 
-        // Log (aún duplicado, NO tocado en este commit)
-        System.out.println("[AUDITORIA] Cambio en sistema realizado el: " + java.time.LocalDate.now());
+        registrarLog();
     }
 
     public void cancelarCita(String paciente, String fecha) {
 
-        
         if (!validarPaciente(paciente)) return;
 
         System.out.println("Cita cancelada para " + paciente);
 
-        // Log (sin refactor todavía)
-        System.out.println("[AUDITORIA] Cambio en sistema realizado el: " + java.time.LocalDate.now());
+        registrarLog();
     }
 
     public void emitirReceta(String paciente, String medicamento) {
 
-        
         if (!validarPaciente(paciente)) return;
 
         System.out.println("Receta de " + medicamento + " para " + paciente);
 
-        // Log (sin refactor todavía)
-        System.out.println("[AUDITORIA] Cambio en sistema realizado el: " + java.time.LocalDate.now());
+        registrarLog();
     }
 
-    
-    // (COMMIT 2)
-    
     private boolean validarPaciente(String paciente) {
         if (paciente == null || paciente.isEmpty()) {
             System.out.println("ERROR: Paciente no válido");
             return false;
         }
         return true;
+    }
+
+    // Commit 3
+    private void registrarLog() {
+        System.out.println("[AUDITORIA] Cambio en sistema realizado el: " + java.time.LocalDate.now());
     }
 }
